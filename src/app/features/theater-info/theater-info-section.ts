590e0a5bd26909479807e0a03dd419b3a0f6bb27
doncorleone <linus@wieland.click>
2025-12-06 22:15:31 +0100

    responsive

--- src/app/features/theater-info/theater-info-section.ts
+++ src/app/features/theater-info/theater-info-section.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, AfterViewInit, ViewChild, ElementRef, ChangeDetectorRef, inject } from '@angular/core';
 import {YouTubePlayer} from '@angular/youtube-player';
 
 @Component({
@@ -8,10 +8,37 @@ import {YouTubePlayer} from '@angular/youtube-player';
   imports: [YouTubePlayer],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TheaterInfoSectionComponent {
+export class TheaterInfoSectionComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('youTubePlayer') youTubePlayer!: ElementRef<HTMLDivElement>;
+
+  videoHeight: number | undefined;
+  videoWidth: number | undefined;
+  changeDetectorRef = inject(ChangeDetectorRef);
+
+  ngOnInit(): void {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(tag);
+  }
   
   onImageError(event: any): void {
     // Fallback to placeholder image if original fails to load
     event.target.src = '/images/placeholder.svg';
+  }
+    ngAfterViewInit(): void {
+    this.onResize();
+    window.addEventListener("resize", this.onResize.bind(this));
+  }
+
+  onResize(): void {
+    // you can remove this line if you want to have wider video player than 1200px
+    this.videoWidth = Math.min(
+      this.youTubePlayer.nativeElement.clientWidth,
+      1320
+    );
+    // so you keep the ratio
+    this.videoHeight = this.videoWidth * 0.6;
+    this.changeDetectorRef.detectChanges();
   }
 }
